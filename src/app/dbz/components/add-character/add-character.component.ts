7a67a8cee37589e528ae31c4d57fee518b6198a2
Diana Pineda <dianipineda55@gmail.com>
2023-05-19 23:33:26 -0500

    ejercicio en clase decorador @Input

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dbz-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css'],
+})
+export class AddCharacterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+  public characters: Character[] = [
+    { name: 'Krilim', power: 1000 },
+    { name: 'Goku', power: 9500 },
+  ];
+}
