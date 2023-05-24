d6615b5a1a32aa7abb12c824e0f868cdebb958e2
Diana Pineda <dianipineda55@gmail.com>
2023-05-23 23:11:16 -0500

    creación de carpeta docs

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
-import { v4 as uuid } from 'uuid'
-
+import { v4 as uuid } from 'uuid';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class DbzService {
   public characters: Character[] = [
     { id: uuid(), name: 'Krilim', power: 1000 },
@@ -15,18 +13,16 @@ export class DbzService {
 
   //uso de desestructuración con operador spread
   onNewCharacte(character: Character): void {
-
-    this.characters.push(character)
+    this.characters.push(character);
   }
   // onDeleteCharacte(index: number){
   //   this.characters.splice(index, 1)
   // }
 
-  onDeleteCharacterById(id: string){
-    this.characters = this.characters.filter(character=>
-      character.id !== id
-    )
+  onDeleteCharacterById(id: string) {
+    this.characters = this.characters.filter(
+      (character) => character.id !== id
+    );
   }
-
 }
 //En los service debe ir la logica del negocio, aca pase las propiedades y metodos, que usare en los componentes que necesite llamando al servicio mediante la inyeccion de independencias
